26595d8700b41eb3cf92c9e40ca8a4353e829723
Александр Федоров <fedorovas@int.adtspb.ru>
2023-03-05 20:33:28 +0300

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,15 @@
 int main()
 {
     crow::SimpleApp app;
-
-    CROW_ROUTE(app, "/")
-    ([]() {
-        return "Hello, world!";
+    
+    CROW_ROUTE(app, "/add/<int>/<int>")
+    ([](crow::response& res, int a, int b) {
+        std::ostringstream os;
+        os << a + b;
+        res.write(os.str());
+        res.end();
     });
+
 
     app.port(80).run();
 }
